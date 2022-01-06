68ca6bff0995677c2fc74ab282366616aaea2fc2
xandjiji <xandjiji@gmail.com>
2022-01-06 09:30:26 -0300

    chore(scripts): adding staging endpoints

--- apps/exevo-pan/src/Constants/Endpoints.ts
+++ apps/exevo-pan/src/Constants/Endpoints.ts
@@ -1,16 +1,28 @@
 const isLocal =
   process.env.NODE_ENV === 'development' && !process.env.NEXT_PUBLIC_FRONT_DEV
 
+const isStaging = process.env.NEXT_PUBLIC_STAGING
+
 const LOCAL_AUCTIONS_ENDPOINT = 'http://localhost:8787'
 const LOCAL_HISTORY_ENDPOINT = 'http://localhost:4000'
+
+const STAGING_AUCTIONS_ENDPOINT = 'https://dev-auctions.service-exevopan.com'
+const STAGING_HISTORY_ENDPOINT = 'https://staging-history-exevopan.loca.lt'
+
+const PRODUCTION_AUCTIONS_ENDPOINT = 'https://auctions.service-exevopan.com'
+const PRODUCTION_HISTORY_ENDPOINT = 'https://history.service-exevopan.com'
 
 export const endpoints = {
   CURRENT_AUCTIONS: isLocal
     ? LOCAL_AUCTIONS_ENDPOINT
-    : 'https://auctions.service-exevopan.com',
+    : isStaging
+    ? STAGING_AUCTIONS_ENDPOINT
+    : PRODUCTION_AUCTIONS_ENDPOINT,
   HISTORY_AUCTIONS: isLocal
     ? LOCAL_HISTORY_ENDPOINT
-    : 'https://history.service-exevopan.com',
+    : isStaging
+    ? STAGING_HISTORY_ENDPOINT
+    : PRODUCTION_HISTORY_ENDPOINT,
   STATIC_DATA: 'https://static-exevopan.surge.sh',
   WAR_DATA: 'https://exevo-pan-war-data.netlify.app',
   TIBIADATA: 'https://api.tibiadata.com/v2/characters',
--- apps/exevo-pan/src/services/Auctions/utils.ts
+++ apps/exevo-pan/src/services/Auctions/utils.ts
@@ -4,6 +4,7 @@ export const buildHeaders = (endpoint: string): Headers => {
   const headers = new Headers()
   if (endpoint === endpoints.HISTORY_AUCTIONS) {
     headers.set('Content-Type', 'application/json')
+    headers.set('Bypass-Tunnel-Reminder', 'true')
   }
   return headers
 }
