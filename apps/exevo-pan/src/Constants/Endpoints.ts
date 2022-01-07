76631c0107596e19811d3d2ee10f2224e1e87fdf
xandjiji <xandjiji@gmail.com>
2022-01-07 08:12:53 -0300

    refactor: enviroment variables

--- apps/exevo-pan/src/Constants/Endpoints.ts
+++ apps/exevo-pan/src/Constants/Endpoints.ts
@@ -1,28 +1,6 @@
-const isLocal =
-  process.env.NODE_ENV === 'development' && !process.env.NEXT_PUBLIC_FRONT_DEV
-
-const isStaging = process.env.NEXT_PUBLIC_STAGING
-
-const LOCAL_AUCTIONS_ENDPOINT = 'http://localhost:8787'
-const LOCAL_HISTORY_ENDPOINT = 'http://localhost:4000'
-
-const STAGING_AUCTIONS_ENDPOINT = 'https://dev-auctions.service-exevopan.com'
-const STAGING_HISTORY_ENDPOINT = 'https://staging-history-exevopan.loca.lt'
-
-const PRODUCTION_AUCTIONS_ENDPOINT = 'https://auctions.service-exevopan.com'
-const PRODUCTION_HISTORY_ENDPOINT = 'https://history.service-exevopan.com'
-
 export const endpoints = {
-  CURRENT_AUCTIONS: isLocal
-    ? LOCAL_AUCTIONS_ENDPOINT
-    : isStaging
-    ? STAGING_AUCTIONS_ENDPOINT
-    : PRODUCTION_AUCTIONS_ENDPOINT,
-  HISTORY_AUCTIONS: isLocal
-    ? LOCAL_HISTORY_ENDPOINT
-    : isStaging
-    ? STAGING_HISTORY_ENDPOINT
-    : PRODUCTION_HISTORY_ENDPOINT,
+  CURRENT_AUCTIONS: process.env.NEXT_PUBLIC_AUCTIONS_ENDPOINT as string,
+  HISTORY_AUCTIONS: process.env.NEXT_PUBLIC_HISTORY_ENDPOINT as string,
   STATIC_DATA: 'https://static-exevopan.surge.sh',
   WAR_DATA: 'https://exevo-pan-war-data.netlify.app',
   TIBIADATA: 'https://api.tibiadata.com/v2/characters',
---
+++ scripts/cleanEnvOverrides.sh
@@ -0,0 +1,1 @@
+rm apps/exevo-pan/.env.development.local
